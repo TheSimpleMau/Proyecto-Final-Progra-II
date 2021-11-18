61a90664023edad1aaf3e42e7c912a2e83cf2ebc
Mauricio Olguin <mauolguisanchez@gmail.com>
2021-11-17 22:39:22 -0600

    sraper terminado

--- scraper.py
+++ scraper.py
@@ -1,9 +1,10 @@
 import requests
 import lxml.html as html
 import os
-import datetime
-import cairosvg
-import urllib.request
+# import datetime
+# import cairosvg
+# import urllib.request
+from string import ascii_letters, digits, punctuation
 from googlesearch import search
 
 
@@ -19,16 +20,50 @@ def getting_info(link:str):
             # print(maths_fomulas)
             cleaned_math_fomula = []
             for formula in maths_fomulas:
-                if formula[0]=='{':
-                    formula = formula.replace('{','')
-                    formula = formula.replace('}','')
-                    fromula = formula.replace('\\\\\\','*')
-                    cleaned_math_fomula.append(formula)
-                else:
-                    pass
+                if len(''.join(formula)) > 5:
+                    if formula[0] != '\n':
+                        formula = formula.replace('\\displaystyle ','')
+                        new_formula = [chars for chars in formula]
+                        if new_formula[0] == '' or new_formula[0] == ' ':
+                            new_formula.pop(0)
+                        itera = 0
+                        for char in new_formula:
+                            if itera == 0 or itera == len(new_formula)-1:
+                                new_formula[itera] = ''
+                            itera += 1
+                        formula = ''.join(new_formula)
+                        for char in formula:
+                            if char not in ascii_letters or char in digits or char in punctuation:
+                                break
+                        if len("".join(formula)) > 5:
+                            cleaned_math_fomula.append(formula)
+                # if '\\displaystyle' in formula:
+                #     formula = formula.replace('\\displaystyle','')
             if len(cleaned_math_fomula)>10:
-                return cleaned_math_fomula[0:9]
-            return cleaned_math_fomula
+                cleaned_math_fomula = cleaned_math_fomula[0:10]
+            ###REVISAR SI FUNCIONA CON DOBLE DIAGONAL INVERTDIDA CON MATPLOTLIB###
+            os.system('ls -a > check_files.txt')
+            with open('check_files.txt','r') as file:
+                files = file.readlines()
+                if '.just_a_forgetable_text.txt\n' not in files:
+                    os.system('touch .just_a_forgetable_text.txt')
+                    with open('.just_a_forgetable_text.txt','w') as file:
+                        file.write(f'LINK:{link}\n')
+                        file.close()
+                file.close()
+            os.system('rm check_files.txt')
+            with open('.just_a_forgetable_text.txt','a') as file:
+                for formula in cleaned_math_fomula:
+                    file.write(formula+'\n')
+            finlas_formulas = []
+            # os.system('open .just_a_forgetable_text.txt')
+            # input()
+            with open('.just_a_forgetable_text.txt','r') as file:
+                for line in file:
+                    line = line.replace('\n','')
+                    finlas_formulas.append(line)
+            return finlas_formulas
+            # return cleaned_math_fomula
             # fullname = ''
             # urllib.request.urlretrieve(image,fullname+'.svg')
             # cairosvg.svg2png(url=f"./{fullname}.svg", write_to=f"./images/{fullname}.png")
@@ -51,15 +86,20 @@ def parse_home(links:list):
             print('Si jala')
             maths_formuls = getting_info(link)
             print(maths_formuls)
+            # print(len(maths_formuls))
+            # print(maths_formuls)
+            # for text in maths_formuls:
+            #     print(text)
         else:
-            print(f'Cannot enter into the website\nError {response.status_code}')
-            return
+            print(f'Cannot enter into the website\nError {response.status_code} on the website.')
+            # return
 
 
 def main():
     to_search = input('Que quieres buscar en wikipedia? ')
     links = getting_links(to_search)
     parse_home(links)
+    os.system('cat .just_a_forgetable_text.txt')
 
 
 if __name__ == '__main__':
