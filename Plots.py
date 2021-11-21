0d36ea4b64196066562882120cc5c4b9c85a1322
Mauricio Olguin <mauolguisanchez@gmail.com>
2021-11-21 01:35:26 -0600

    Pequenos cambios a Plots

--- Plots.py
+++ Plots.py
@@ -8,6 +8,7 @@ class Plots:
         self.busqueda = busqueda
         self.checking_files()
         self.graph()
+        self.dir_name = f'Images_{self.busqueda.replace(" ","")}/'
 
 
     def checking_files(self):
@@ -17,7 +18,7 @@ class Plots:
 
 
     def graph(self):
-        to_show = '$'+self.text+'$'
+        to_show = '$'+self.text+'$' #self.text.replace(" ","")
         #Limpia la ventana de la grafica
         plt.clf()
         plt.title("LaTeX plot")
@@ -30,7 +31,12 @@ class Plots:
             plt.yticks([])
             archivos = os.listdir(f'Images_{self.busqueda.replace(" ","")}')
             if self.text+'.png' not in archivos:
-                plt.savefig(f'Images_{self.busqueda.replace(" ","")}/'+self.text+'.png')
+                try:
+                    plt.savefig(f'Images_{self.busqueda.replace(" ","")}/'+self.text.replace(" ","")+'.png',dpi=100)
+                except Exception:
+                    plt.savefig(f'Images_{self.busqueda.replace(" ","")}\\'+self.text.replace(" ","")+'.png',dpi=100)
+                else:
+                    raise Exception
         except Exception:
             print(f'Error en formula: {self.text}')
             pass
